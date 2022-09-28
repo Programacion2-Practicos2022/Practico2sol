0c1023cba4f9172c49ce426afc9d0a14e5829b79
Elizabeth Izquierdo <73123352+eliizquierdo@users.noreply.github.com>
2022-09-28 23:06:04 +0000

    Agregue calculadora

---
+++ Practico2/src/logica/Calculadora.java
@@ -0,0 +1,16 @@
+package logica;
+
+public class Calculadora {
+	
+	public Calculadora() {
+	} // Constructor por defecto
+	
+	public int suma(int x, int y) {
+		return (x + y);
+	} // Fin del mÃ©todo suma
+	
+	public int resta(int x, int y) {
+		return (x - y);
+	} // Fin del mÃ©todo resta
+	
+} // Fin de la clase Calculadora
